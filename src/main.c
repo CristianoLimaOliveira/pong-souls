c623a6c71ba3cbfe55a1f397460e79b62f76c9db
Glenio <gleniolobo2009@gmail.com>
2021-11-27 19:02:56 -0300

    teste mapa

--- src/main.c
+++ src/main.c
@@ -3,17 +3,26 @@
 #include "..\assets\barra.c"
 #include "..\assets\bola.c"
 #include "..\assets\parede.c"
+#include "..\assets-test\testeback.c"
+#include "..\assets-test\testeback2.c"
+#include "..\assets-test\mapteste.c"
+#include "..\assets-test\mapteste2.c"
+#include "..\assets-test\mapteste3.c"
 
 void main(){
     UINT8 currentspriteindex = 0;
-    UINT8 limiteVerSup=80, limiteVerInf=-80;
 
     SPRITES_8x8;
     /* set_sprite_data: (Tile de início, a quantidade de tiles do arquivo que vai carregar, data)
+     * Tile de início é a posição do VRAM do gamebou onde ele vai começar, todos os outros tiles vao ser colocados a partir dele.
      * set_sprite_tile: (Um valor que você quer atribuir ao sprite, tile que voce definiu no set_sprite_data)
      * mov_sprite: move
+     * set_bkg_data(Tile de inicio (Esse vai ser o da tela do gameboy em si), quantidade de tiles 
+     *  necessárias para o background, a função asset dos tiles)
+     * set_bkg_tiles(X inicial, y incial, x final, y final, mapa)
      * Limites: Horizontal: De 8 em 8 até 20*8 e vertical de 16 em 8 até 18*8+8
     */
+
     set_sprite_data(0, 1, Barra);
     set_sprite_tile(3, 0);
     move_sprite(3, 0, 0);
@@ -28,7 +37,18 @@ void main(){
 
     SHOW_SPRITES;
 
-    /*while(1){
+    set_bkg_data(0, 21, BackTeste2); // Load os sprites que compõem o background na memória
+    set_bkg_tiles(0, 0, 20, 18, MapTeste3);
+
+    SHOW_BKG;
+    DISPLAY_ON;
+    
+    delay(100);
+    set_bkg_data(0, 3, BackTest); // Load os sprites que compõem o background na memória
+    set_bkg_tiles(0, 0, 20, 18, MapTeste2);
+
+    /*
+    while(1){
         if(currentspriteindex==0){
             currentspriteindex = 1;
         }
