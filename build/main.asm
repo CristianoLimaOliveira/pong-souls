2d61d98be308267e21e9d79450dd65dcad9d5c82
Glenio <gleniolobo2009@gmail.com>
2021-11-27 15:58:33 -0300

    teste

--- build/main.asm
+++ build/main.asm
@@ -9,7 +9,10 @@
 ; Public variables in this module
 ;--------------------------------------------------------
 	.globl _main
-	.globl _printf
+	.globl _set_sprite_data
+	.globl _Parede
+	.globl _Bola
+	.globl _Barra
 ;--------------------------------------------------------
 ; special function registers
 ;--------------------------------------------------------
@@ -21,6 +24,12 @@
 ; ram data
 ;--------------------------------------------------------
 	.area _INITIALIZED
+_Barra::
+	.ds 16
+_Bola::
+	.ds 16
+_Parede::
+	.ds 16
 ;--------------------------------------------------------
 ; absolute external ram data
 ;--------------------------------------------------------
@@ -41,21 +50,123 @@
 ; code
 ;--------------------------------------------------------
 	.area _CODE
-;..\src\main.c:4: void main(){
+;..\src\main.c:7: void main(){
 ;	---------------------------------
 ; Function main
 ; ---------------------------------
 _main::
-;..\src\main.c:5: printf("PONG SOULS");
-	ld	de, #___str_0
+;..\src\main.c:11: SPRITES_8x8;
+	ldh	a, (_LCDC_REG + 0)
+	and	a, #0xfb
+	ldh	(_LCDC_REG + 0), a
+;..\src\main.c:16: set_sprite_data(0, 1, Barra);
+	ld	de, #_Barra
 	push	de
-	call	_printf
-	pop	hl
-;..\src\main.c:6: }
+	xor	a, a
+	inc	a
+	push	af
+	call	_set_sprite_data
+	add	sp, #4
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1326: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 14)
+	ld	(hl), #0x00
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1399: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	bc, #_shadow_OAM+12
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1400: itm->y=y, itm->x=x;
+	xor	a, a
+	ld	(bc), a
+	inc	bc
+	xor	a, a
+	ld	(bc), a
+;..\src\main.c:20: set_sprite_data(1, 1, Bola);
+	ld	de, #_Bola
+	push	de
+	ld	hl, #0x101
+	push	hl
+	call	_set_sprite_data
+	add	sp, #4
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1326: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 6)
+	ld	(hl), #0x01
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1399: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	hl, #(_shadow_OAM + 4)
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1400: itm->y=y, itm->x=x;
+	ld	a, #0x10
+	ld	(hl+), a
+	ld	(hl), #0x58
+;..\src\main.c:24: set_sprite_data(2, 1, Parede);
+	ld	de, #_Parede
+	push	de
+	ld	hl, #0x102
+	push	hl
+	call	_set_sprite_data
+	add	sp, #4
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1326: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 10)
+	ld	(hl), #0x02
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1399: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	hl, #(_shadow_OAM + 8)
+;C:/Users/Glenio Lobo/Desktop/Faculdade/Interface Hardware Software/pong-souls/gbdk/include/gb/gb.h:1400: itm->y=y, itm->x=x;
+	ld	a, #0x98
+	ld	(hl+), a
+	ld	(hl), #0x50
+;..\src\main.c:28: SHOW_SPRITES;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x02
+	ldh	(_LCDC_REG + 0), a
+;..\src\main.c:51: }
 	ret
-___str_0:
-	.ascii "PONG SOULS"
-	.db 0x00
 	.area _CODE
 	.area _INITIALIZER
+__xinit__Barra:
+	.db #0x18	; 24
+	.db #0x1c	; 28
+	.db #0x18	; 24
+	.db #0x1c	; 28
+	.db #0x18	; 24
+	.db #0x1c	; 28
+	.db #0x18	; 24
+	.db #0x1c	; 28
+	.db #0x18	; 24
+	.db #0x1c	; 28
+	.db #0x18	; 24
+	.db #0x1c	; 28
+	.db #0x18	; 24
+	.db #0x1c	; 28
+	.db #0x18	; 24
+	.db #0x1c	; 28
+__xinit__Bola:
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x3c	; 60
+	.db #0x3c	; 60
+	.db #0x7e	; 126
+	.db #0x7e	; 126
+	.db #0x7e	; 126
+	.db #0x7e	; 126
+	.db #0x7e	; 126
+	.db #0x7e	; 126
+	.db #0x7e	; 126
+	.db #0x7e	; 126
+	.db #0x3c	; 60
+	.db #0x3c	; 60
+	.db #0x00	; 0
+	.db #0x00	; 0
+__xinit__Parede:
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0x08	; 8
+	.db #0xff	; 255
+	.db #0xff	; 255
+	.db #0x00	; 0
+	.db #0x00	; 0
 	.area _CABS (ABS)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,52 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "..\assets\barra.c"
+#include "..\assets\bola.c"
+#include "..\assets\parede.c"
 
 void main(){
-    printf("PONG SOULS");
+    UINT8 currentspriteindex = 0;
+    UINT8 limiteVerSup=80, limiteVerInf=-80;
+
+    SPRITES_8x8;
+    /* set_sprite_data: (Tile de início, a quantidade de tiles do arquivo que vai carregar, data)
+     * set_sprite_tile: (Um valor que você quer atribuir ao sprite, tile que voce definiu no set_sprite_data)
+     * mov_sprite: move
+     * Limites: Horizontal: De 8 em 8 até 20*8 e vertical de 16 em 8 até 18*8+8
+    */
+    set_sprite_data(0, 1, Barra);
+    set_sprite_tile(3, 0);
+    move_sprite(3, 0, 0);
+
+    set_sprite_data(1, 1, Bola);
+    set_sprite_tile(1, 1);
+    move_sprite(1, 88, 16);
+
+    set_sprite_data(2, 1, Parede);
+    set_sprite_tile(2, 2);
+    move_sprite(2, 20*4,144+8);
+
+    SHOW_SPRITES;
+
+    /*while(1){
+        if(currentspriteindex==0){
+            currentspriteindex = 1;
+        }
+        else{
+            currentspriteindex = 0;
+        }
+        set_sprite_tile(0, currentspriteindex);
+        switch(joypad()){
+            case J_UP:
+                scroll_sprite(0,0,-10);
+                break;
+            case J_DOWN:
+                scroll_sprite(0,0,10);
+                break;
+        }
+
+
+        delay(100);
+        scroll_sprite(0,10,0);
+    }*/
 }
