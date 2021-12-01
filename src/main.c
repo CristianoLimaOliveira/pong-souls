e410ca57642a46126d5a24e2b1101ae2389eb26e
Cristiano Lima Oliveira <cristianolo@dcomp.ufs.br>
2021-12-01 17:09:46 -0300

    bolas, barras e contorno da quadra implementados

--- src/main.c
+++ src/main.c
@@ -1,16 +1,67 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "..\assets\barra.c"
 #include "..\assets\bola.c"
 #include "..\assets\parede.c"
+#include "..\assets\backgroundLabirinto.c"
+#include "..\assets\backgroundLabTiles.c"
 #include "..\assets-test\testeback.c"
 #include "..\assets-test\testeback2.c"
 #include "..\assets-test\mapteste.c"
 #include "..\assets-test\mapteste2.c"
 #include "..\assets-test\mapteste3.c"
 
+const unsigned char smile[] =
+{
+  0x3C,0x3C,0x42,0x42,0x81,0x81,0xA5,0xA5,
+  0x81,0x81,0x81,0xA5,0x42,0x5A,0x3C,0x3C
+};
+
+const char blankmap[1] = {0x00};
+UINT8 positionBarraRightX[] = {152,152,152};
+UINT8 positionBarraRightY[] = {136,128,120};
+UINT8 transitionBarraRight = 0;
+
+UBYTE canplayermove(UINT8 newplayerx, UINT8 newplayery){
+    UINT16 indexTLx, indexTLy, tileindexTL;
+    UBYTE result;
+
+    indexTLx = (newplayerx - 8) / 8;
+    indexTLy = (newplayery - 16) / 8;
+    tileindexTL = 20 * indexTLy + indexTLx;
+
+    result = backgroundLabirinto[tileindexTL] == 0x00; //0x00 indica os locais do background sem tiles.
+
+    return result;
+}
+
+void moveBarraRight(){
+    if(!(canplayermove(positionBarraRightX[0], positionBarraRightY[0]-8) && canplayermove(positionBarraRightX[1], positionBarraRightY[1]-8) && canplayermove(positionBarraRightX[2], positionBarraRightY[2]-8))){
+        transitionBarraRight=1;
+    }
+    if(!(canplayermove(positionBarraRightX[0], positionBarraRightY[0]+8) && canplayermove(positionBarraRightX[1], positionBarraRightY[1]+8) && canplayermove(positionBarraRightX[2], positionBarraRightY[2]+8))){
+        transitionBarraRight=0;
+    }
+    if(transitionBarraRight==0){
+        for(UINT8 x=0; x<3; x++){
+            positionBarraRightY[x]-=8;
+            move_sprite(x+4,positionBarraRightX[x],positionBarraRightY[x]);
+        }
+    }
+    if(transitionBarraRight==1){
+        for(UINT8 x=0; x<3; x++){
+            positionBarraRightY[x]+=8;
+            move_sprite(x+4,positionBarraRightX[x],positionBarraRightY[x]);
+        }
+    }
+}
+
+void moveBolaUm(){}
+
+void moveBolaDois(){}
+
 void main(){
-    UINT8 currentspriteindex = 0;
 
     SPRITES_8x8;
     /* set_sprite_data: (Tile de início, a quantidade de tiles do arquivo que vai carregar, data)
@@ -23,7 +74,7 @@ void main(){
      * Limites: Horizontal: De 8 em 8 até 20*8 e vertical de 16 em 8 até 18*8+8
     */
 
-    set_sprite_data(0, 1, Barra);
+    /*set_sprite_data(0, 1, Barra);
     set_sprite_tile(3, 0);
     move_sprite(3, 0, 0);
 
@@ -35,38 +86,87 @@ void main(){
     set_sprite_tile(2, 2);
     move_sprite(2, 20*4,144+8);
 
-    SHOW_SPRITES;
+    SHOW_SPRITES;*/
 
     set_bkg_data(0, 21, BackTeste2); // Load os sprites que compõem o background na memória
     set_bkg_tiles(0, 0, 20, 18, MapTeste3);
+    SHOW_BKG;
 
+    while(joypad()!=J_START){}
+
+    set_bkg_data(0, 4, backgroundLabTiles);
+    set_bkg_tiles(0, 0, 20, 18, backgroundLabirinto);
+
+    UINT8 positionBall[] = {48,24};
+    UINT8 positionBallTwo[] = {64,112};
+    UINT8 positionBarraLeftX[] = {16,16,16};
+    UINT8 positionBarraLeftY[] = {32,40,48};
+
+    // Bola Um
+	set_sprite_data(0, 1, Bola);
+    // Barra Esquerda
+    set_sprite_data(1, 1, Barra);
+    set_sprite_data(2, 1, Barra);
+    set_sprite_data(3, 1, Barra);
+    // Barra Direita
+    set_sprite_data(4, 1, Barra);
+    set_sprite_data(5, 1, Barra);
+    set_sprite_data(6, 1, Barra);
+    // Bola Dois
+	set_sprite_data(7, 1, Bola);
+
+    // Meio que serve para associar o desenho do tile que está na VRAM com a referência que vamos usar para manipular o tile no código.
+	set_sprite_tile(0, 0); // primeiro valor é a referência (Nb) definida para o dado através do primeiro campo do sprite_data, e o segundo valor é o indice do desenho do tile na VRAM.
+    set_sprite_tile(1, 1);
+    set_sprite_tile(2, 2);
+    set_sprite_tile(3, 3);
+    set_sprite_tile(4, 4);
+    set_sprite_tile(5, 5);
+    set_sprite_tile(6, 6);
+    set_sprite_tile(7, 7);
+
+	move_sprite(0, positionBall[0], positionBall[1]);
+	move_sprite(1, positionBarraLeftX[0], positionBarraLeftY[0]);
+    move_sprite(2, positionBarraLeftX[1], positionBarraLeftY[1]);
+    move_sprite(3, positionBarraLeftX[2], positionBarraLeftY[2]);
+    move_sprite(4, positionBarraRightX[0], positionBarraRightY[0]);
+    move_sprite(5, positionBarraRightX[1], positionBarraRightY[1]);
+    move_sprite(6, positionBarraRightX[2], positionBarraRightY[2]);
+    move_sprite(7, positionBallTwo[0], positionBallTwo[1]);
+
+
+	SHOW_SPRITES;
     SHOW_BKG;
     DISPLAY_ON;
-    
-    delay(100);
-    set_bkg_data(0, 3, BackTest); // Load os sprites que compõem o background na memória
-    set_bkg_tiles(0, 0, 20, 18, MapTeste2);
 
-    /*
-    while(1){
-        if(currentspriteindex==0){
-            currentspriteindex = 1;
-        }
-        else{
-            currentspriteindex = 0;
-        }
-        set_sprite_tile(0, currentspriteindex);
-        switch(joypad()){
-            case J_UP:
-                scroll_sprite(0,0,-10);
-                break;
-            case J_DOWN:
-                scroll_sprite(0,0,10);
-                break;
-        }
+	while(1){
+		if (joypad() & J_UP)
+		{ 
+			if(canplayermove(positionBarraLeftX[0], positionBarraLeftY[0]-8) && canplayermove(positionBarraLeftX[1], positionBarraLeftY[1]-8) && canplayermove(positionBarraLeftX[2], positionBarraLeftY[2]-8)){
+                for(UINT8 x=0; x<3; x++){
+                    positionBarraLeftY[x]-=8;
+			        move_sprite(x+1,positionBarraLeftX[x],positionBarraLeftY[x]);
+                }
+            }
+		}
+		else if (joypad() & J_DOWN)
+		{ 
+			if(canplayermove(positionBarraLeftX[0], positionBarraLeftY[0]+8) && canplayermove(positionBarraLeftX[1], positionBarraLeftY[1]+8) && canplayermove(positionBarraLeftX[2], positionBarraLeftY[2]+8)){
+                for(UINT8 x=0; x<3; x++){
+                    positionBarraLeftY[x]+=8;
+			        move_sprite(x+1,positionBarraLeftX[x],positionBarraLeftY[x]);
+                }
+            }
+		}
 
+        moveBarraRight();
 
-        delay(100);
-        scroll_sprite(0,10,0);
-    }*/
+        moveBolaUm();
+
+        moveBolaDois();
+
+        for(UINT8 i=0; i<6; i++){
+		    wait_vbl_done();
+        }
+	}
 }
--- src/make.bat
+++ src/make.bat
@@ -2,4 +2,3 @@ mkdir ..\build
 cd ..\build
 ..\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o ..\src\main.c
 ..\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
-pause
