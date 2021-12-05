b75db01061846b79ab7878f30cf15e013ef3314e
Glenio <gleniolobo2009@gmail.com>
2021-12-05 02:39:28 -0300

    Alteração no código de colisão, colisão das bolas

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <rand.h>
-#include <stdint.h>
 #include "..\assets\barraLongaPreta.c"
 #include "..\assets\bola.c"
 #include "..\assets\parede.c"
 #include "..\assets\backgroundPong.c"
 #include "..\assets\linhaDaQuadra.c"
-#include "..\assets-test\testeback.c"
 #include "..\assets-test\testeback2.c"
-#include "..\assets-test\mapteste.c"
-#include "..\assets-test\mapteste2.c"
 #include "..\assets-test\mapteste3.c"
+
+#define TRUE 1
+#define FALSE 0
+
+// Bola um começa com o x e o y aumentando.
+// Bola dois começa com o x diminuindo e o y aumentando.
+INT16 BALL_ONE_XVEL = 8;
+INT16 BALL_ONE_YVEL = 8;
+INT16 BALL_TWO_XVEL = -8;
+INT16 BALL_TWO_YVEL = 8;
 
 typedef struct Ball {
 	UINT16 spritid;
@@ -21,7 +25,6 @@ typedef struct Ball {
 	UINT16 y;
 	UINT16 width;
 	UINT16 height;
-    UINT16 direction;
     UINT16 latestPlayer; // serve para indica qual player vai receber pontuação dos blocos quebrados por ter sido o último player a bater na bola.
 } Ball;
 
@@ -33,188 +36,80 @@ typedef struct Barra {
 	UINT16 height;
 } BarraGame;
 
-UINT16 transitionBarraRight = 0;
+UBYTE vetorBarrasRemovidas[360];
 
-UINT16 sortearUmValor(){
-    uint16_t seed = LY_REG;
-    seed |= (uint16_t)DIV_REG << 8;
-    initrand(seed);
-    return ((UINT16)rand()) % (UINT16)3;
+/*
+* Se a direção for 0 é para cima, se for 1 é para baixo.
+*/
+UBYTE canPlayerMove(UINT16 topy, UINT16 bottomy){
+    if(topy > 16 && bottomy <= 17*8+8){
+        return TRUE;
+    }
+    return FALSE;
 }
 
-UINT16 canplayermove(UINT16 newplayerx, UINT16 newplayery){
-    UINT16 indexTLx, indexTLy, tileindexTL;
-    UBYTE result;
 
-    indexTLx = (newplayerx - 8) / 8;
-    indexTLy = (newplayery - 16) / 8;
+UBYTE checkBallCollisions(INT16 x, INT16 y){
+    INT16 indexTLx, indexTLy, tileindexTL;
+    INT16 teste_tile[1];
+
+    indexTLx = (x - 8) / 8;
+    indexTLy = (y - 16) / 8;
     tileindexTL = 20 * indexTLy + indexTLx;
+    UINT8 next_cell[1];
+    get_bkg_tiles(indexTLx, indexTLy, 1, 1, next_cell);
 
-    result = BackgroundPong[tileindexTL] == 0x00; //0x00 indica os locais do background sem tiles.
+    if(vetorBarrasRemovidas[tileindexTL] == TRUE){
+        return FALSE;
+    }
 
-    return result;
+    if(BackgroundPong[tileindexTL] == 0x07){
+        unsigned char blank_tile[1] = {0x00};
+        set_bkg_tiles(indexTLx, indexTLy, 1, 1, blank_tile);
+        vetorBarrasRemovidas[tileindexTL] = TRUE;
+    }
+
+    return BackgroundPong[tileindexTL] != 0;
 }
 
 void moveBola(Ball* selectedBall){
-    UINT16 resultSorteio=sortearUmValor();
+    /*
+        *  INT8 BALL_ONE_XVEL = 1;
+        *  INT8 BALL_ONE_YVEL = 1;
+        *  INT8 BALL_TWO_XVEL = -1;
+        *  INT8 BALL_TWO_YVEL = 1;
+    */
 
-    if(selectedBall->direction == 0 && !canplayermove(selectedBall->x, (selectedBall->y-8))){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==0 && canplayermove((selectedBall->x-8), (selectedBall->y+8))){
-            selectedBall->direction = 3;
-        } else if(resultSorteio==1 && canplayermove(selectedBall->x, (selectedBall->y+8))){
-            selectedBall->direction = 4;
-        }else if(resultSorteio==2 && canplayermove((selectedBall->x+8), (selectedBall->y+8))){
-            selectedBall->direction = 5;
-        } else if(canplayermove((selectedBall->x-8), (selectedBall->y+8))){
-            selectedBall->direction = 3;
-        } else if(canplayermove(selectedBall->x, (selectedBall->y+8))){
-            selectedBall->direction = 4;
-        }else if(canplayermove((selectedBall->x+8), (selectedBall->y+8))){
-            selectedBall->direction = 5;
-        }
-    } else if(selectedBall->direction == 1 && !canplayermove((selectedBall->x-8), (selectedBall->y-8))){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==0 && canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        } else if(resultSorteio==1 && canplayermove((selectedBall->x-8), (selectedBall->y+8))){
-            selectedBall->direction = 3;
-        } else if(canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        } else if(canplayermove((selectedBall->x-8), (selectedBall->y+8))){
-            selectedBall->direction = 3;
-        }else if(canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        }
-    } else if(selectedBall->direction == 2 && !canplayermove((selectedBall->x-8), selectedBall->y)){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==0 && canplayermove((selectedBall->x+8), (selectedBall->y+8))){
-            selectedBall->direction = 5;
-        } else if(resultSorteio==1 && canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        }else if(resultSorteio==2 && canplayermove((selectedBall->x+8), (selectedBall->y-8))){
-            selectedBall->direction = 7;
-        } else if(canplayermove((selectedBall->x+8), (selectedBall->y+8))){
-            selectedBall->direction = 5;
-        } else if(canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        }else if(canplayermove((selectedBall->x+8), (selectedBall->y-8))){
-            selectedBall->direction = 7;
-        }
-    } else if(selectedBall->direction == 3 && !canplayermove((selectedBall->x-8), (selectedBall->y+8))){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==0 && canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        } else if(resultSorteio==1 && canplayermove((selectedBall->x-8), (selectedBall->y-8))){
-            selectedBall->direction = 1;
-        } else if(canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        } else if(canplayermove((selectedBall->x-8), (selectedBall->y-8))){
-            selectedBall->direction = 1;
-        }else if(canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        }
-    } else if(selectedBall->direction == 4 && !canplayermove(selectedBall->x, (selectedBall->y+8))){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==0 && canplayermove((selectedBall->x+8), (selectedBall->y-8))){
-            selectedBall->direction = 7;
-        } else if(resultSorteio==1 && canplayermove(selectedBall->x, (selectedBall->y-8))){
-            selectedBall->direction = 0;
-        }else if(resultSorteio==2 && canplayermove((selectedBall->x-8), (selectedBall->y-8))){
-            selectedBall->direction = 1;
-        } else if(canplayermove((selectedBall->x+8), (selectedBall->y-8))){
-            selectedBall->direction = 7;
-        } else if(canplayermove(selectedBall->x, (selectedBall->y-8))){
-            selectedBall->direction = 0;
-        }else if(canplayermove((selectedBall->x-8), (selectedBall->y-8))){
-            selectedBall->direction = 1;
-        }
-    } else if(selectedBall->direction == 5 && !canplayermove((selectedBall->x+8), (selectedBall->y+8))){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==1 && canplayermove((selectedBall->x+8), (selectedBall->y-8))){
-            selectedBall->direction = 7;
-        }else if(resultSorteio==2 && canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        } else if(canplayermove((selectedBall->x+8), (selectedBall->y-8))){
-            selectedBall->direction = 7;
-        }else if(canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        } else if(canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        }
-    } else if(selectedBall->direction == 6 && !canplayermove((selectedBall->x+8), selectedBall->y)){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==0 && canplayermove((selectedBall->x-8), (selectedBall->y-8))){
-            selectedBall->direction = 1;
-        } else if(resultSorteio==1 && canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        }else if(resultSorteio==2 && canplayermove((selectedBall->x-8), (selectedBall->y+8))){
-            selectedBall->direction = 3;
-        } else if(canplayermove((selectedBall->x-8), (selectedBall->y-8))){
-            selectedBall->direction = 1;
-        } else if(canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        }else if(canplayermove((selectedBall->x-8), (selectedBall->y+8))){
-            selectedBall->direction = 3;
-        }
-    } else if(selectedBall->direction == 7 && !canplayermove((selectedBall->x+8), (selectedBall->y-8))){
-        resultSorteio=sortearUmValor();
-        if(resultSorteio==1 && canplayermove((selectedBall->x+8), (selectedBall->y+8))){
-            selectedBall->direction = 5;
-        }else if(resultSorteio==2 && canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        } else if(canplayermove((selectedBall->x+8), (selectedBall->y+8))){
-            selectedBall->direction = 5;
-        }else if(canplayermove((selectedBall->x+8), selectedBall->y)){
-            selectedBall->direction = 6;
-        }else if(canplayermove((selectedBall->x-8), selectedBall->y)){
-            selectedBall->direction = 2;
-        }
-    }
+    if(selectedBall->spritid==0){
 
-    if(selectedBall->direction == 0){
-        selectedBall->y -= 8;
-    } else if(selectedBall->direction == 1){
-        selectedBall->x -= 8;
-        selectedBall->y -= 8;
-    } else if(selectedBall->direction == 2){
-        selectedBall->x -= 8;
-    } else if(selectedBall->direction == 3){
-        selectedBall->x -= 8;
-        selectedBall->y += 8;
-    } else if(selectedBall->direction == 4){
-        selectedBall->y += 8;
-    } else if(selectedBall->direction == 5){
-        selectedBall->x += 8;
-        selectedBall->y += 8;
-    } else if(selectedBall->direction == 6){
-        selectedBall->x += 8;
-    } else if(selectedBall->direction == 7){
-        selectedBall->x += 8;
-        selectedBall->y -= 8;
-    }
+        if(checkBallCollisions(selectedBall->x+BALL_ONE_XVEL, selectedBall->y)){
+            BALL_ONE_XVEL = -BALL_ONE_XVEL;
 
-    move_sprite(selectedBall->spritid, selectedBall->x, selectedBall->y);
-}
+        }
 
-void moveBarraRight(BarraGame* selectedBarra){
-    if(!(canplayermove(selectedBarra->x[0], selectedBarra->y[0]-8) && canplayermove(selectedBarra->x[1], selectedBarra->y[1]-8) && canplayermove(selectedBarra->x[2], selectedBarra->y[2]-8))){
-        transitionBarraRight=1; // Serve para identificar se a barra direita tem que continuar subindo ou descendo.
-    }
-    if(!(canplayermove(selectedBarra->x[0], selectedBarra->y[0]+8) && canplayermove(selectedBarra->x[1], selectedBarra->y[1]+8) && canplayermove(selectedBarra->x[2], selectedBarra->y[2]+8))){
-        transitionBarraRight=0;
-    }
-    if(transitionBarraRight==0){
-        for(UINT16 x=0; x<3; x++){
-            selectedBarra->y[x]-=8;
-            move_sprite(x+4,selectedBarra->x[x],selectedBarra->y[x]);
+        if(checkBallCollisions(selectedBall->x, selectedBall->y+BALL_ONE_YVEL)){
+            BALL_ONE_YVEL = -BALL_ONE_YVEL;
         }
-    }
-    if(transitionBarraRight==1){
-        for(UINT16 x=0; x<3; x++){
-            selectedBarra->y[x]+=8;
-            move_sprite(x+4,selectedBarra->x[x],selectedBarra->y[x]);
+        
+        selectedBall->x += BALL_ONE_XVEL;
+        selectedBall->y += BALL_ONE_YVEL;
+        move_sprite(selectedBall->spritid, selectedBall->x, selectedBall->y);
+
+    }else{
+
+        if(checkBallCollisions(selectedBall->x+BALL_TWO_XVEL, selectedBall->y)){
+            BALL_TWO_XVEL = -BALL_TWO_XVEL;
         }
+
+        if(checkBallCollisions(selectedBall->x, selectedBall->y+BALL_TWO_YVEL)){
+            BALL_TWO_YVEL = -BALL_TWO_YVEL;
+        }
+
+        selectedBall->x += BALL_TWO_XVEL;
+        selectedBall->y += BALL_TWO_YVEL;
+        move_sprite(selectedBall->spritid, selectedBall->x, selectedBall->y);
+
+
     }
 }
 
@@ -222,6 +117,8 @@ void main(){
 
     SPRITES_8x8;
     BarraGame barraEsq, barraDir;
+    Ball ballOne, ballTwo;
+
     barraDir.spritid[0] = 4; barraDir.spritid[1] = 5; barraDir.spritid[2] = 6;
     barraDir.x[0] = 152; barraDir.x[1] = 152; barraDir.x[2] = 152;
     barraDir.y[0] = 136; barraDir.y[1] = 128; barraDir.y[2] = 120;
@@ -232,16 +129,14 @@ void main(){
     barraEsq.y[0] = 32; barraEsq.y[1] = 40; barraEsq.y[2] = 48;
     barraEsq.width = 8; barraEsq.height = 8;
 
-    Ball ballOne, ballTwo;
     ballOne.spritid = 0;
     ballOne.x = 48; ballOne.y = 24;
     ballOne.width = 8; ballOne.height = 8;
-    ballOne.direction = 2;
 
     ballTwo.spritid = 7;
     ballTwo.x = 120; ballTwo.y = 64;
     ballTwo.width = 8; ballTwo.height = 8;
-    ballTwo.direction = 6;
+
     /* set_sprite_data: (Tile de início, a quantidade de tiles do arquivo que vai carregar, data)
      * Tile de início é a posição do VRAM do gamebou onde ele vai começar, todos os outros tiles vao ser colocados a partir dele.
      * set_sprite_tile: (Um valor que você quer atribuir ao sprite, tile que voce definiu no set_sprite_data)
@@ -301,8 +196,8 @@ void main(){
 	while(1){
 		if (joypad() & J_UP)
 		{ 
-			if(canplayermove(barraEsq.x[0], barraEsq.y[0]-8) && canplayermove(barraEsq.x[1], barraEsq.y[1]-8) && canplayermove(barraEsq.x[2], barraEsq.y[2]-8)){
-                for(UINT16 x=0; x<3; x++){
+            if(canPlayerMove(barraEsq.y[0]-8, barraEsq.y[2]-8)){
+                for(UINT8 x=0; x<3; x++){
                     barraEsq.y[x]-=8;
 			        move_sprite(x+1,barraEsq.x[x],barraEsq.y[x]);
                 }
@@ -310,16 +205,14 @@ void main(){
 		}
 		else if (joypad() & J_DOWN)
 		{ 
-			if(canplayermove(barraEsq.x[0], barraEsq.y[0]+8) && canplayermove(barraEsq.x[1], barraEsq.y[1]+8) && canplayermove(barraEsq.x[2], barraEsq.y[2]+8)){
-                for(UINT16 x=0; x<3; x++){
+            if(canPlayerMove(barraEsq.y[0]+8, barraEsq.y[2]+8)){
+                for(UINT8 x=0; x<3; x++){
                     barraEsq.y[x]+=8;
 			        move_sprite(x+1,barraEsq.x[x],barraEsq.y[x]);
                 }
             }
 		}
-
-        moveBarraRight(&barraDir);
-
+        
         moveBola(&ballOne);
 
         moveBola(&ballTwo);
